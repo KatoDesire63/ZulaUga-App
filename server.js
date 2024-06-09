d143e0a036a9bf392580b02f66928bce6c8da7f0
Kato Deisire <113764597+KatoDesire63@users.noreply.github.com>
2024-06-09 08:45:06 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const momgoose = require('mongoose');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
@@ -7,39 +7,35 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-
-//Middleware
+// Middleware
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static(path.join(_dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-
-//Connect to MongoDB
+// Connect to MongoDB
 mongoose.connect('mongodb+srv://katodesire9:Ur2x6F6eNk9DHepY.@zulauga.kk5tmgu.mongodb.net/?retryWrites=true&w=majority&appName=Zulauga', {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
 
-
-//Routes
+// Routes
 const testimonialsRoutes = require('./routes/testimonials');
 const bookingsRoutes = require('./routes/bookings');
 
 app.use('/api/testimonials', testimonialsRoutes);
 app.use('/api/bookings', bookingsRoutes);
 
-
-//Serve static HTML files
+// Serve static HTML files
 app.get('/', (req, res) => {
-	res.sendFile(path.join(_dirname,'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/book-now', (req, res) => {
-	res.sendFile(path.join(_dirname, 'public', 'book-mow.html'));
+    res.sendFile(path.join(__dirname, 'public', 'book-now.html'));
 });
 
 app.listen(port, () => {
-	console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
